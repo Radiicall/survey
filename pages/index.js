0d3ffa4dcd1b342c66fc965c4725f23e97be69d3
Radical <benjaminjohnsen.bgj@gmail.com>
2022-02-21 15:01:44 +0100

    killme

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,34 @@
-import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script';
 
 export default function Home() {
   const answer = event => {
     event.preventDefault();
 
-    console.log(answer.toString());
+    const name = document.getElementById("name").value;
+    const exp = document.getElementById("exp").value;
+    console.log(name, exp)
   }
   return (
     <>
-      <Head>
-        <title>Spørreundersøkelse</title>
-      </Head>
       <h1>Spørreundersøkelse</h1>
       <form onSubmit={answer}>
         <p>Name:</p>
-        <label htmlFor="name" />
         <input id="name" type="text" placeholder='Ex: John Doe' autoComplete="name" required />
+        <p>d</p>
+        <label htmlFor="Experience">Rate your Experience with the program.</label>
+        <select defaultValue={"5"} name="exp" id="exp">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
         <button type="submit">Send Response</button>
       </form>
       <footer>
