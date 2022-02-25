f0fa9caa20f6dcb91a0758d445a6c874d7e07d41
Radiicall <66682497+Radiicall@users.noreply.github.com>
2022-02-25 13:47:36 +0100

    This shit stupid

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import '../styles/global.css'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,50 @@
 import Link from 'next/link'
-import Script from 'next/script';
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const answer = event => {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const exp = document.getElementById("exp").value;
-    console.log(name, exp)
-  }
   return (
     <>
-      <h1>Spørreundersøkelse</h1>
-      <form onSubmit={answer}>
-        <p>Name:</p>
-        <input id="name" type="text" placeholder='Ex: John Doe' autoComplete="name" required />
-        <p>d</p>
-        <label htmlFor="Experience">Rate your Experience with the program.</label>
-        <select defaultValue={"5"} name="exp" id="exp">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-        <button type="submit">Send Response</button>
-      </form>
-      <footer>
+      <h1>Survey</h1>
+      <div>
+        <form>
+          <label>Name *:</label><br /><br />
+          <input className={styles.input} id="name" type="text" placeholder='Ex: John Doe' autoComplete="name" required /><br /><br /><br />
+          <label>Email *:</label><br /><br />
+          <input className={styles.input} id="email" type="text" placeholder='email@email.com' autoComplete='email' /><br /><br /><br />
+          <label htmlFor="Experience">Rate your Experience with the program *:</label><br /><br />
+          <select required className={styles.button} defaultValue={"5"} name="exp" id="exp">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select><br /><br /><br />
+          <label>Why did you delete the program? *</label><br /><br />
+          <select 
+          required
+          className={styles.button} 
+          name="why" 
+          id="why">
+            <option value="I didn't like it">I didn&apos;t like it</option>
+            <option value="It was too expensive">It was too expensive</option>
+            <option value="I didn't mean to!">I didn&apos;t mean to!</option>
+            <option value="The program was too hard to use">The program was too hard to use</option>
+            <option value="It was taking up a lot of storage">It was taking up a lot of storage</option>
+            <option value="Other">Other</option>
+          </select><br /><br /><br />
+          <label>Did you have any other issues?</label><br /><br />
+          <textarea className={styles.textarea} minLength={50} cols={40} rows={5}></textarea><br /><br /><br />
+          <button className={styles.button} type="submit">Send Response</button>
+        </form>
+      </div>
+      <footer><br /><br />
         Created by{' '}
-        <Link href="https://github.com/Radiicall">Radical</Link>
+        <Link href="https://github.com/Radiicall">Benjamin</Link>
       </footer>
     </>
   )
